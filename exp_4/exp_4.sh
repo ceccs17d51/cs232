f304e44a3bd9d08ddf494e26eb551db20bcdeebe
ceccs17d51 <35443067+ceccs17d51@users.noreply.github.com>
2019-05-12 20:23:16 +0530

    Add files via upload

---
+++ exp_4/exp_4.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+logname        ## current logname ##
+echo $USER     ##current logged user ##
+echo $SHELL    ## current shell ##
+echo $OSTYPE   ## os type ##
+cd	       ## home directory ##
+echo $PATH     ## current path ##
+pwd            ## current working directory ##
+users | wc -w  ## no of users currently logged in ##
+
+
